43eef90d50dccae8b31a966fdaa3c7335fc8fae8
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-06-16 08:52:04 +0300

    fix urls

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(3000, "127.0.0.1",() => {
-  console.log("Server running on 127.0.0.1");
+app.get("/image", (req, res) => {
+  res.redirect("/image.jpg");
+});
+
+app.listen(3000, () => {
+  console.log("🚀 Server running on http://localhost:3000");
 });
